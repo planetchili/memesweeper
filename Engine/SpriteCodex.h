7cf4426e8cba208cdaa36e5e8c8673738e69a10f
planetchili <planetchili@users.noreply.github.com>
2017-02-26 17:07:32 +0900

    added win sprite and lose sound

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -27,4 +27,6 @@ public:
 	static void DrawTileBombRed( const Vei2& pos,Graphics& gfx );
 	// Tile selector function valid input 0-8
 	static void DrawTileNumber( const Vei2& pos,int n,Graphics& gfx );
+	// Win Screen 254x192 center origin
+	static void DrawWin( const Vei2& pos,Graphics& gfx );
 };
